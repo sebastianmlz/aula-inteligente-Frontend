3bbabfca076e2eddd6b2732657e24c72572e3bbc
Victoroide <cvictorhugo39@gmail.com>
2025-05-22 01:08:45 -0400

    Refactor server.js to streamline static file serving and improve error handling

--- server.js
+++ server.js
@@ -6,15 +6,11 @@ const distFolder = path.join(__dirname, 'dist', 'aula-inteligente-frontend');
 
 app.use(express.static(distFolder));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(distFolder, 'index.html'), function(err) {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
+app.use('*', (req, res) => {
+  res.sendFile(path.join(distFolder, 'index.html'));
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
